95a5dfe8a21979fe68ad0d1cebc444c5e06cfc1b
laboon <laboon@gmail.com>
2014-02-05 14:51:11 -0500

    Added FizzBuzzTest and ReflectionFun sample code

--- sample_code/FizzBuzz.java
+++ sample_code/FizzBuzz.java
@@ -2,6 +2,30 @@ package com.laboon;
 
 public class FizzBuzz {
 
+	private boolean buzzy(int n) {
+		return (n % 5 == 0);
+	}
+	
+	private boolean fizzy(int n) {
+		return (n % 3 == 0);
+	}
+	
+	public String value(int n) {
+		if (fizzy(n) && buzzy(n)) {
+			return "FizzBuzz";
+		} else if  (fizzy(n)) {
+			return "Fizz";
+		} else if (buzzy(n)) {
+			return "Buzz";
+		} else {
+			return String.valueOf(n);
+		}
+	}
+	
+	public void run() {
+		
+	}
+	
 	public static void main(String[] args) {
 		for (int j=1; j<=100; j++) {
 			if (j % 3 == 0 && j % 5 == 0) {
